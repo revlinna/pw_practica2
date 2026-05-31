150079c6f6484535fdc55518c4804bfd25eaa2c4
revlinna <pzvereva@uoc.edu>
2026-05-31 20:05:50 +0200

    Add files via upload

---
+++ listas.js
@@ -0,0 +1,21 @@
+/**
+ * listas.js — Gestión de las listas del usuario (Viendo actualmente / Plan to Watch)
+ */
+
+let currentUser;
+let activeListKey = 'watching'; // Lista activa por defecto
+
+/* Añadir las funciones que consideréis necesarias*/
+
+
+/**
+ * Crear la tarjeta de un anime dentro de la lista.
+ */
+
+
+/**
+ * Eliminar un anime de la lista activa y actualiza la vista.
+ */
+
+
+/* Añadir las funciones que consideréis necesarias*/
